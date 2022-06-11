1987c77f281d24abab314c094c34fc9cf06335bf
Cola <bcad1591@gmail.com>
2022-06-12 02:00:07 +0900

    Feat: index.js 기본 세팅

--- simple2/frontend/src/index.js
+++ simple2/frontend/src/index.js
@@ -1,48 +1,12 @@
+import Router from './Router/index.js';
 import Home from './routes/Home.js';
 import About from './routes/About.js';
-import NotFound from './routes/NotFound.js';
 
-const router = async () => {
-  const routes = [
-    { path: '/', view: Home },
-    { path: '/about', view: About },
-  ];
+const $router = document.querySelector('#router');
+const $page = document.querySelector('#page');
+const routes = [
+  { path: '/', view: Home },
+  { path: '/about', view: About },
+];
 
-  const potentialMatches = routes.map((route) => {
-    return {
-      route: route,
-      isMatch: location.pathname === route.path,
-    };
-  });
-
-  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
-
-  if (!match) {
-    match = {
-      route: { path: null, view: NotFound },
-    };
-  }
-
-  const $root = document.querySelector('#root');
-  $root.innerHTML = match.route.view();
-};
-
-const navigate = (url) => {
-  history.pushState({}, null, url);
-  router();
-};
-
-document.body.addEventListener('click', (e) => {
-  if (e.target.matches('.route')) {
-    e.preventDefault();
-    navigate(e.target.href);
-  }
-});
-
-window.addEventListener('popstate', () => {
-  router();
-});
-
-window.addEventListener('DOMContentLoaded', () => {
-  router();
-});
+new Router({ $router, $page, routes });
